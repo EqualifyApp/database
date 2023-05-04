428efa83f09eba0382af56469d9b7ff3ece4c4fb
Bentley Hensel <bentley.hensel@civicactions.com>
2023-05-04 21:01:45 +0000

    Schema?

--- dockherd/child_a11yphant.sql
+++ dockherd/child_a11yphant.sql
@@ -2,13 +2,8 @@
 
 -- Import Data
     -- FROM: start_domains TO: targets.domains
-    \copy targets.domains(id, domain, active)
-    FROM '/docker-entrypoint-initdb.d/start_domains.csv'
-    DELIMITER ','
-    CSV HEADER;
+    \copy targets.domains(id, domain, active) FROM '/docker-entrypoint-initdb.d/start_domains.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
 
     -- FROM: start_urls TO: targets.urls
-    \copy targets.urls(url, domain_id, is_objective)
-    FROM '/docker-entrypoint-initdb.d/start_urls.csv'
-    DELIMITER ','
-    CSV HEADER;
+    \copy targets.urls(url, domain_id, is_objective) FROM '/docker-entrypoint-initdb.d/start_urls.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+
