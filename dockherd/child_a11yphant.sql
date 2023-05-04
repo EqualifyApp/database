237df20b5072618ed2ffb2b56940c3bbdd987a11
Bentley Hensel <bentley.hensel@civicactions.com>
2023-05-04 20:52:24 +0000

    - COPY —> /copy - File location

--- dockherd/child_a11yphant.sql
+++ dockherd/child_a11yphant.sql
@@ -2,13 +2,13 @@
 
 -- Import Data
     -- FROM: start_domains TO: targets.domains
-    COPY targets.domains(id, domain, active)
-    FROM '../docker-entrypoint-initdb.d/start_domains.csv'
+    \copy targets.domains(id, domain, active)
+    FROM '/docker-entrypoint-initdb.d/start_domains.csv'
     DELIMITER ','
     CSV HEADER;
 
     -- FROM: start_urls TO: targets.urls
-    COPY targets.urls(url, domain_id, is_objective)
-    FROM '../docker-entrypoint-initdb.d/start_urls.csv'
+    \copy targets.urls(url, domain_id, is_objective)
+    FROM '/docker-entrypoint-initdb.d/start_urls.csv'
     DELIMITER ','
     CSV HEADER;
